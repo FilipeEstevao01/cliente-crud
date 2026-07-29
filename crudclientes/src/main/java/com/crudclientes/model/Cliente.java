52e1dcf06599aedec74c0d63c09ad52ca43fa3bb
Filipe <filipedev0@gmail.com>
2026-07-29 19:08:46 -0300

    Foi criado dentro da tabela a coluna email

--- crudclientes/src/main/java/com/crudclientes/model/Cliente.java
+++ crudclientes/src/main/java/com/crudclientes/model/Cliente.java
@@ -1,4 +1,23 @@
 package com.crudclientes.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+
+import javax.swing.text.html.parser.Entity;
+
+@Entity
+@Table
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+
 }
